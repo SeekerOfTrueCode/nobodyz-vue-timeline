9045d8d1c41054b597d13098f28dbaf60564de4b
Wojciech Borawski <borawskiwojciech@wp.pl>
2020-03-07 22:58:50 +0100

    - exported types fix (visible outside the lib with TS)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,8 @@
-declare module "vue-nobodyz-timeline" {
-    // eslint-disable-next-line no-unused-vars
-    // import Vue from "vue";
-    export const Timeline: any;
-    export const TimelineRow: any;
-    export const TimelineRowRects: any;
+// eslint-disable-next-line no-unused-vars
+import { VueConstructor } from "vue";
 
-    // const _Timeline: {
-    //     Timeline: Vue,
-    //     TimelineRow: Vue,
-    //     TimelineRowRects: Vue,
-    // };
-    // export default _Timeline;
-}
+declare module "vue-nobodyz-timeline";
+
+export const Timeline: VueConstructor<Vue>;
+export const TimelineRow: VueConstructor<Vue>;
+export const TimelineRowRects: VueConstructor<Vue>;
